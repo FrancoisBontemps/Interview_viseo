a80affd66c5114815eecdb226048d1d401d5620e
leopalazzi <leopalazzi@outlook.fr>
2019-03-21 14:48:12 +0100

    Context + Firebase (#3)

--- react_project/src/App.js
+++ react_project/src/App.js
@@ -10,6 +10,19 @@ import ResumeAllChapter from './ResumeAllChap';
 
 const data = require('./Interview');
 
+var firebase = require('firebase');
+var config = {
+    apiKey: 'AIzaSyDmRPV5SUPIW_oFQj9xweTMHAXLbPA4imU',
+    authDomain: 'viseo-36d12.firebaseapp.com',
+    databaseURL: 'https://viseo-36d12.firebaseio.com',
+    projectId: 'viseo-36d12',
+    storageBucket: 'viseo-36d12.appspot.com',
+    messagingSenderId: '131746919965'
+};
+firebase.initializeApp(config);
+
+let obj_chapter = {};
+
 class App extends React.Component {
     state = {
         renderForm: true,
@@ -21,8 +34,10 @@ class App extends React.Component {
         chapterIndex: 1,
         sectionIndex: 0,
         note: [],
-        appreciations: []
+        appreciations: [],
+        userName: ''
     };
+
     handleFormUnmount = () => {
         this.setState({ renderForm: false, renderChapter: true, renderResume: false });
     };
@@ -41,6 +56,12 @@ class App extends React.Component {
                 renderResume: true
             });
         }
+    };
+
+    handleUserNameChanged = userName => {
+        this.setState({
+            username: userName
+        });
     };
 
     handleResumeUnmount = () => {
@@ -74,10 +95,46 @@ class App extends React.Component {
                     sectionIndex={sectionIndex}
                     chapterIndex={chapterIndex}
                     SectionUnmount={this.handleSectionUnmount}
+                    username={this.state.username}
+                    chapnum={this.state.chapterIndex}
                 />
             </div>
         );
     }
+    gotData = donnes => {
+        const { sectionIndex, chapterIndex, note } = this.state;
+        console.log('Chapitre index courant : ' + chapterIndex);
+        if (this.state.username && data.chapters['chapter' + chapterIndex].sections.length === sectionIndex) {
+            console.log(donnes.val());
+            obj_chapter = donnes.val();
+            let numNote = new Array();
+            for (let i = 1; i <= data.chapters['chapter' + chapterIndex].sections.length; i++) {
+                numNote.push(obj_chapter['section' + i]);
+                console.log(numNote);
+            }
+
+            let moy = numNote.reduce(function(acc, val) {
+                return acc + parseInt(val);
+            }, 0);
+            if (numNote.length === 0) {
+                moy = 0;
+            } else {
+                moy = moy / numNote.length;
+            }
+
+            if (chapterIndex < Object.keys(data.chapters).length + 1) {
+                if (data.chapters['chapter' + chapterIndex].sections.length === sectionIndex) {
+                    if (!isNaN(moy)) note.push(moy);
+                }
+            }
+            console.log(obj_chapter);
+            console.log(note);
+        }
+    };
+
+    errData = err => {
+        console.log(err);
+    };
     render() {
         const {
             renderForm,
@@ -92,32 +149,49 @@ class App extends React.Component {
             appreciations
         } = this.state;
         let numNote = new Array();
-        for (let i = 1; i <= data.chapters['chapter' + chapterIndex].sections.length; i++) {
-            numNote.push(localStorage.getItem('section' + i));
-        }
-
-        let moy = numNote.reduce(function(acc, val) {
-            return acc + parseInt(val);
-        }, 0);
-        if (numNote.length === 0) {
-            moy = 0;
-        } else {
-            moy = moy / numNote.length;
-        }
-
-        if (chapterIndex < Object.keys(data.chapters).length + 1) {
-            if (data.chapters['chapter' + chapterIndex].sections.length === sectionIndex) {
-                note.push(moy);
+        var str_db = 'student/' + this.state.username + '/chapter' + this.state.chapterIndex.toString();
+        var ref = firebase.database().ref(str_db);
+        ref.on('value', this.gotData, this.errData);
+        /*
+            for (let i = 1; i <= data.chapters['chapter' + chapterIndex].sections.length; i++) {
+                numNote.push(obj_chapter['section' + i]);
+                console.log(numNote);
             }
-        }
+
+            let moy = numNote.reduce(function(acc, val) {
+                return acc + parseInt(val);
+            }, 0);
+            if (numNote.length === 0) {
+                moy = 0;
+            } else {
+                moy = moy / numNote.length;
+            }
+
+            if (chapterIndex < Object.keys(data.chapters).length + 1) {
+                if (data.chapters['chapter' + chapterIndex].sections.length === sectionIndex) {
+                    note.push(moy);
+                }
+            }*/
+
+        console.log('noteee :' + note[chapterIndex - 1]);
         return (
             <div className="App">
+                <p> {note[chapterIndex - 1]}</p>
                 <header className="NameForm">
                     <img src={logo} className="App-logo" />
-                    <div className="Form">{renderForm ? <NameForm FormUnmount={this.handleFormUnmount} /> : null}</div>
+                    <div className="Form">
+                        {renderForm ? (
+                            <NameForm FormUnmount={this.handleFormUnmount} setUsername={this.handleUserNameChanged} />
+                        ) : null}
+                    </div>
                     <div className="Chapter">
                         {renderChapter && chapterIndex < Object.keys(data.chapters).length + 1 ? (
-                            <Chapter chapnum={chapterIndex} data={data} unmountChapter={this.handleChapterUnmount} />
+                            <Chapter
+                                chapnum={chapterIndex}
+                                data={data}
+                                unmountChapter={this.handleChapterUnmount}
+                                username={this.state.username}
+                            />
                         ) : null}
                     </div>
                     <div className="Section">
@@ -136,6 +210,7 @@ class App extends React.Component {
                                 chapnum={chapterIndex}
                                 ResumeUnmount={this.handleResumeUnmount}
                                 moyenne={note[chapterIndex - 1]}
+                                username={this.state.username}
                             />
                         ) : null}
                     </div>
